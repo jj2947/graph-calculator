7fa15d70643314b6bb6f18b2f5328fa72ba17db3
Jessica Jiang <jessicaj2947@gmail.com>
2023-05-28 09:42:57 +1200

    added linkedlist class that i completed in coderunner exercise

---
+++ src/main/java/nz/ac/auckland/se281/datastructures/LinkedList.java
@@ -0,0 +1,105 @@
+package nz.ac.auckland.se281.datastructures;
+
+public class LinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public T get(int index) {
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.getNext();
+        }
+        return current.getData();
+    }
+
+    public void insert(int index, T data) {
+
+        if (index == 0) {
+            Node<T> newNode = new Node<>(data);
+            newNode.setNext(head);
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getNext();
+            }
+            Node<T> newNode = new Node<>(data);
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }
+        size++;
+    }
+
+    public void remove(int index) {
+
+        if (index == 0) {
+            head = head.getNext();
+        } else {
+            Node<T> current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.getNext();
+            }
+            current.setNext(current.getNext().getNext());
+        }
+        size--;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int indexOf(T data) {
+        Node<T> current = head;
+        int index = 0;
+
+        while (current != null) {
+            if (current.getData().equals(data)) {
+                return index;
+            }
+            current = current.getNext();
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<T> current = head;
+        while (current != null) {
+            sb.append(current.getData());
+            if (current.getNext() != null) {
+                sb.append(", ");
+            }
+            current = current.getNext();
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
